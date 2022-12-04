34ca50b327310e0b5c7bf311a966f4431608fc3d
Yvonne213 <hywener@gmail.com>
2022-12-03 23:07:17 -0600

    fixed

--- InCircleArtist/main.js
+++ InCircleArtist/main.js
@@ -172,19 +172,18 @@ async function main() {
     }
   }
 
-  checkNFT.onclick = async function() {
-    let tokenBalance = await contract.balanceOf(connectedWalletAddres);
-    console.log(+tokenBalance);
-    tokenBalance = +tokenBalance;
-    if(tokenBalance < 1) {
-      sorry.style.display = "block";
-    } else {
-      windows.open("https://yvonne213.github.io/Projects/InCircleArtist/check","width=900px, height=260px")
-      VIP.textContent = "VIP: " + connectedWalletAddres;
-    }
-  }
-
 }
 
+checkNFT.onclick = async function() {
+  let tokenBalance = await contract.balanceOf(connectedWalletAddres);
+  console.log(+tokenBalance);
+  tokenBalance = +tokenBalance;
+  if(tokenBalance < 1) {
+    sorry.style.display = "block";
+  } else {
+    windows.open("https://yvonne213.github.io/Projects/InCircleArtist/check","width=900px, height=260px")
+    VIP.textContent = "VIP: " + connectedWalletAddres;
+  }
+}
 // EVENT LISTENERS
 
