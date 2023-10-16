9e0cad2898e0dbdf1ef8136d12db244b89f0dec6
Yvonne <hywener@gmail.com>
2023-10-16 02:35:25 -0500

    update new sketch.js

--- open-studio-night/sketch.js
+++ open-studio-night/sketch.js
@@ -4,32 +4,21 @@ let timer;
 let minTime = 200;
 //maximum time limit
 let maxTime = 500;
-
 let notify = true;
-let w = 0;
-let y = 0;
 let alpha = 0;
 //let extraCanvas;
-
-
+let xOffset = 0;
+let yOffset = 1000;
 
 var song;
-var song1;
-var song2;
-var song3;
-
 
 //preload sound effect
 function preload() {
   song = loadSound("8407.mp3");
-  song1=loadSound("2221.mp3");
-  song2=loadSound("33.mp3");
-  song3=loadSound("4.mp3");
-  
 }
 
 function setup() {
-  createCanvas(1024, 1366);//770 790
+  createCanvas(1024, 1366); //770 790
   let rand = random(5, 15);
   let x = random(width);
   let y = random(height);
@@ -38,8 +27,32 @@ function setup() {
   print(timer);
 }
 
+
 function draw() {
-  background(255); // Clear the canvas
+  // background(255); // Clear the canvas
+  // let r = map(mouseX, 0, width, 0, 255);
+  // let g = map(mouseY, 0, height, 0, 255);
+  // let b = map(mouseX, width, 0, 0, 255);
+  // background(r, g, b);
+
+  // Set background with a flowing water effect using Perlin noise
+  //255,r,100
+  loadPixels();
+  for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
+      let index = (x + y * width) * 4;
+      let r = noise(x * 0.01 + xOffset, y * 0.01 + yOffset) * 255;
+      pixels[index] = 255; // Red channel
+      pixels[index + 1] = r; //Green channel
+      pixels[index + 2] = 100; //Blue
+      pixels[index + 3] = 180; // Alpha channel (fully opaque)
+    }
+  }
+  updatePixels();
+
+  // Update Perlin noise offsets to create a flowing effect
+  xOffset += 0.01;
+  yOffset += 0.01;
 
   let m = millis();
   //this is the random timer that triggers a new notification
@@ -56,6 +69,7 @@ function draw() {
   }
 }
 
+
 function createNotification() {
   let a = random(0, 10);
   // this sets a random alpha transparency
@@ -65,11 +79,13 @@ function createNotification() {
   } else {
     alpha = 0;
   }
+
   // this is where we are creating a new notification
   let i = round(random(0, 8)) * 2; // aligning with the grid pattern of rectangles
   let j = round(random(0, 11)) * 2; // aligning with the grid pattern of rectangles
   notifications.push(new Notification(138 + 50 * i, 132 + 50 * j, 20, alpha));
 }
+
 
 function mousePressed() {
   // Iterate through notifications and flag the ones to be removed
@@ -83,10 +99,33 @@ function mousePressed() {
   for (let i = notifications.length - 1; i >= 0; i--) {
     if (notifications[i].toRemove) {
       notifications.splice(i, 1);
+      if (random(1) > 0.5) {
+        createRandomAlert();
+      }
     }
   }
 }
 
+function createRandomAlert() {
+  let alerts = [
+    "New updates available! Tap to see what's new in version 2.0.",
+    "天猫：双十一，百万大奖等你拿！",
+    "Only spend 1 dollar, get iphone15!",
+    "おっとー勉強の時間だ、ちっちと始めよう！",
+    "Sender:Yuwen\nMessage Preview:\nHi there!\nTake your time, enjoy the notifications!",
+    "Event: Open Studio Night.\nTime: Oct 21-22\nLocation: Sullivan Building 1415",
+    "Reminder Text: Yuwen's studio is in Lakeview 1116.\nCheck it out!",
+    "Greetings from Yvonne:\nIsn't it annoying?",
+    "Sender:yhuang45@saic.edu\nSubject Line:Welcome to RED. exhibition",
+    "Breaking News: Earthquake reported in neighboring city. Follow for updates.",
+    "Reminder: Artist Statement and Bio due tomorrow.",
+    "Today's Forecast: Sunny with a high of 75°F. Perfect day to spend outdoors!",
+    // Add more unique alerts as needed
+  ];
+
+  let randomAlert = random(alerts);
+  alert(randomAlert);
+}
 
 function Notification(x, y, r, alpha) {
   this.x = x;
@@ -106,31 +145,63 @@ function Notification(x, y, r, alpha) {
   };
 }
 
-
-
 function createRect() {
   let b = round(random(255));
   push();
 
   textAlign(CENTER);
-    textSize(20);
-    fill(0);
-  text("©️Yvonne", width/2, height-30);
-  fill(0);
-    textSize(20);
-   text("ATS", width-80, 50);
-  fill(255, 0, b);
-  textSize(80);
-  text(' Error!  Storage is being full!' , 200, 50);
+  textSize(20);
+  fill(190, 100, 100);
+  text("©️Yvonne2023 @hywener", width / 2, height - 30);
 
+  fill(0, 80);
+  textSize(20);
+  text("///////AT/SP", width - 80, 50);
+  fill(255, b, b);
+  textSize(65);
+  // text("Storage Almost full!", 500, 100);
+
+  // words colum
+  let s = 20;
+  let p = 50;
+  let rr=120
+  const scream = "Storage Almost full!";
+  textSize(80);
+  textWrap(WORD);
+  text(scream, -120 - s, 60 - p, 1000);
+  fill(0, 0, 0, 75);
+  // text(scream, -80-s, 100-p,1000)
+  // fill(0, 0, 0, 60);
+  // text(scream, -40-s, 150-p, 1000);
+  // fill(0, 0, 0, 55);
+  // text(scream, 0-s, 200-p, 1000);
+
+  fill(rr, 0, 0, 65);
+  text(scream, 40 - s, 250 - p, 1000);
+  fill(rr, 0, 0, 45);
+  text(scream, 80 - s, 300 - p, 1000);
+  fill(rr, 0, 0, 40);
+  text(scream, 120 - s, 350 - p, 1000);
+  fill(rr, 0, 0, 35);
+  text(scream, 160 - s, 400 - p, 1000);
+  fill(rr, 0, 0, 30);
+  text(scream, 200 - s, 450 - p, 1000);
+  fill(rr, 0, 0, 25);
+  text(scream, 240 - s, 500 - p, 1000);
+  fill(rr, 0, 0, 20);
+  text(scream, 280 - s, 550 - p, 1000);
+  fill(rr, 0, 0, 15);
+  text(scream, 320 - s, 600 - p, 1000);
+  fill(rr, 0, 0, 10);
+  text(scream, 360 - s, 650 - p, 1000);
 
   //rect
-  for (var i = 0; i < 17; i+=2) {
-    for (var j = 0; j < 23; j+=2) {
-      fill(0, 80);
-      stroke(0);
+  for (var i = 0; i < 17; i += 2) {
+    for (var j = 0; j < 23; j += 2) {
+      fill(255, 150);
+      noStroke();
       strokeWeight(1);
-      rect(80+ 50 * i, 130 + 50 * j, 60, 60, 15);
+      rect(80 + 50 * i, 130 + 50 * j, 60, 60, 15);
     }
   }
   pop();
